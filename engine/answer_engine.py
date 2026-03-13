be480ad6634be810d5bea7c111507986e9b21a80
Rahul Dwivedi <51382525+rahuldwvdi@users.noreply.github.com>
2026-03-13 07:24:45 +0530

    message

--- engine/answer_engine.py
+++ engine/answer_engine.py
@@ -1,28 +1,20 @@
+import os
 from groq import Groq
 
 client = Groq(
-    api_key="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"
+    api_key=os.getenv("GROQ_API_KEY")
 )
 
 MODEL = "llama-3.1-8b-instant"
-
 
 def generate_response(prompt: str):
 
     response = client.chat.completions.create(
         model=MODEL,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        max_tokens=40,
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=30,
         temperature=0.3
     )
 
     text = response.choices[0].message.content
-
-    text = text.split("\n")[0]
-
-    return text.strip()
+    return text.split("\n")[0].strip()
